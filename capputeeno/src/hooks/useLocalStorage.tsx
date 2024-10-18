9a7262105626e0a589c420b516b9dc377308be9d
Fabiano Souza <fabianokurashi@gmail.com>
2024-10-17 21:27:22 -0300

    feat: configuring header and localstorage variebles

--- capputeeno/src/hooks/useLocalStorage.tsx
+++ capputeeno/src/hooks/useLocalStorage.tsx
@@ -1,21 +1,25 @@
 import { useEffect, useState } from "react";
 
-export function useLocalStorage<T>(item: string, initialValue: T){
-    const [value, setValue] = useState<T>(initialValue)
+export function useLocalStorage<T>(item: string, initialValue: T) {
+    const [value, setValue] = useState<T>(initialValue);
 
     useEffect(() => {
-        if (typeof window === 'undefined') return;
-        let value = localStorage.getItem(item)
-        if(value) setValue(JSON.parse(value))
-    }, [window])
+        if (typeof window !== 'undefined') {
+            const storedValue = localStorage.getItem(item);
+            if (storedValue) {
+                setValue(JSON.parse(storedValue));
+            } else {
+                setValue(initialValue);
+            }
+        }
+    }, []);
 
     const updateLocalStorage = (newValue: T) => {
         setValue(newValue);
-        localStorage.setItem(item,JSON.stringify(newValue));
-    }
+        if (typeof window !== 'undefined') {
+            localStorage.setItem(item, JSON.stringify(newValue));
+        }
+    };
 
-    return {
-        value,
-        updateLocalStorage
-    }
+    return { value, updateLocalStorage };
 }
